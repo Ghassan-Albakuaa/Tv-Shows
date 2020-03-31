dd693cf569439bb67f25438a22fb4ebbf4d93580
ghassan <gh09an@gmail.com>
2020-03-31 19:39:48 -0400

    added the models

---
+++ Tv-Shows/Backend/Tv-Shows/Tv-Shows/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tv_Shows.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public string Rating { get; set; }
+    }
+}
---
+++ Tv-Shows/Backend/Tv-Shows/Tv-Shows/Models/Tvshow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tv_Shows.Models
+{
+    public class Tvshow
+    {  
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Content { get; set; }
+    }
+}
