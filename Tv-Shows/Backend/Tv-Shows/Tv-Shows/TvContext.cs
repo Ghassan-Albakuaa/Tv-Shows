d3eafc9225528936214e07749731c592c008a02a
ghassan <gh09an@gmail.com>
2020-03-31 20:38:49 -0400

    added tvshowcontext

---
+++ Tv-Shows/Backend/Tv-Shows/Tv-Shows/TvContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tv_Shows.Models;
+
+namespace Tv_Shows
+{
+    public class TvContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tvshow> TvShows { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=TvShowsDb;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
+   
