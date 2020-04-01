4eeca9137e81733df7e0f9485d8a8ad243ea65b7
ghassan <gh09an@gmail.com>
2020-03-31 20:29:42 -0400

    added some staff to startup

--- Tv-Shows/Backend/Tv-Shows/Tv-Shows/Startup.cs
+++ Tv-Shows/Backend/Tv-Shows/Tv-Shows/Startup.cs
@@ -9,9 +9,45 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Tv_Shows
 {
+    //public class Startup
+    //{
+    //    public Startup(IConfiguration configuration)
+    //    {
+    //        Configuration = configuration;
+    //    }
+
+    //    public IConfiguration Configuration { get; }
+
+    //    // This method gets called by the runtime. Use this method to add services to the container.
+    //    public void ConfigureServices(IServiceCollection services)
+    //    {
+    //        services.AddControllers();
+    //        services.AddMvc();
+    //    }
+
+    //    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    //    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    //    {
+    //        if (env.IsDevelopment())
+    //        {
+    //            app.UseDeveloperExceptionPage();
+    //        }
+
+    //        app.UseRouting();
+
+    //        app.UseAuthorization();
+
+    //        app.UseEndpoints(endpoints =>
+    //        {
+    //            endpoints.MapControllers();
+    //        });
+    //    }
+    //}
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -19,13 +55,38 @@ namespace Tv_Shows
             Configuration = configuration;
         }
 
+        // .net 3.1 for cors issue
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            //services.AddControllers().AddNewtonsoftJson();
+            services.AddControllers().AddNewtonsoftJson(o =>
+            {
+                o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+
             services.AddMvc();
+            //services.AddDbContext<Albumcollectioncontext>();
+            //services.AddScoped<IRepository<Album>, AlbumRepository>();
+            //services.AddScoped<IRepository<Artist>, ArtistRepository>();
+            //services.AddScoped<IRepository<Song>, SongRepository>();
+
+            // .net 3.1 for cors issue
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:8080",
+                                        "https://localhost:8080")
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +96,14 @@ namespace Tv_Shows
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            // .net 3.1 cors issue
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
@@ -47,3 +116,5 @@ namespace Tv_Shows
         }
     }
 }
+
+
