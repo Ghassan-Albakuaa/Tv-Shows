f3074379bb4b8cca418b3c6b77e537c708877959
ghassan <gh09an@gmail.com>
2020-04-02 09:07:03 -0400

    create ITvshowRepository

--- Tv-Shows/Backend/Tv-Shows/Tv-Shows/Repositories/ITvshowRepository.cs
+++ Tv-Shows/Backend/Tv-Shows/Tv-Shows/Repositories/ITvshowRepository.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tv_Shows.Models;
 
 namespace Tv_Shows.Repositories
 {
-    public class ITvshowRepository
+    public interface ITvshowRepository
     {
+        ICollection<Tvshow> GetTvshows();
+        Tvshow GetTvshow(int tvshowId);
+        ICollection<Tvshow> GetAllTvshowsForUser(int userId);
+        ICollection<User> GetAllUsersForTvshow(int tvshowId);
+        bool TvshowExists(int tvshowId);
+        bool IsDuplicateTvshowName(int tvshowId, string tvshowName);
+
+        bool CreateTvshow(Tvshow tvshow);
+        bool UpdateTvshow(Tvshow tvshow);
+        bool DeleteTvshow(Tvshow tvshow);
+        bool Save();
     }
 }
