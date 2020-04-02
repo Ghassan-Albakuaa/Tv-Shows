2a9bc3a7f8e535850d6106969340ffb8ae4863b7
ghassan <gh09an@gmail.com>
2020-04-02 07:01:00 -0400

    add iuserrepo

---
+++ Tv-Shows/Backend/Tv-Shows/Tv-Shows/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tv_Shows.Models;
+
+namespace Tv_Shows.Repositories
+{
+    public interface UserRepository
+    {
+        ICollection<User> GetUsers();
+        User GetUser(int userId);
+        User GetUser(string userIsbn);
+        decimal GetUserRating(int userId);
+        bool UserExists(int userId);
+        bool UserExists(string userIsbn);
+        bool IsDuplicateIsbn(int userId, string userIsbn);
+        bool CreateUser(List<int> authorsId, List<int> categoriesId, User user);
+        bool UpdateUser(List<int> authorsId, List<int> categoriesId, User user);
+        bool DeleteUser(User user);
+        bool Save();
+    }
+}
